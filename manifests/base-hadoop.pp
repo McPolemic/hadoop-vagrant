a0eee68a9fc5ca65c99befff25978887d687654d
Adam Lukens <spawn968@gmail.com>
2013-04-21 13:49:34 -0400

    Hadoop up and running with three workers

--- manifests/base-hadoop.pp
+++ manifests/base-hadoop.pp
@@ -5,10 +5,41 @@ group { "puppet":
 }
 
 exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
+  command => 'apt-get update',
+  path    => '/usr/bin'
 }
 
 package { "openjdk-6-jdk":
    ensure => present,
    require => Exec['apt-get update']
 }
+
+file { "/root/.ssh":
+  ensure => "directory",
+  require => Exec['apt-get update']
+}
+
+file { "/root/.ssh/id_rsa":
+  source => "puppet:///modules/hadoop/id_rsa",
+  mode => 600,
+  owner => root,
+  group => root,
+  require => File["/root/.ssh"]
+}
+
+file { "/root/.ssh/id_rsa.pub":
+  source => "puppet:///modules/hadoop/id_rsa.pub",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => File["/root/.ssh"]
+}
+
+ssh_authorized_key {"ssh_key":
+  ensure => "present",
+  key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDFewB2BlmahcXBmQFQFFl0yFHq+w157D2fvXYsCyx+eI5PKTGqToGudYBKlh4F6qhQYELRnh+lxrxVaL/LFq0CfaecBfdiec7SfqdFMYTe22wTTxz0ligZLryrcjgO2FpaH0OYMyZPgmDCwycr5I24y1IOUN78PRl9BRgI22rvZ+doH4BHcNBNHQPDui2aWxB7p4ur6qt9lo6OL3dbYiU6SRgo/9/QGJ4B2oEHP2x4rMcXXzHWKF8ZsHRwnHOjpjbzzs7P5oOZplTmFVtDf5yQ/wM7h/rPlo6BRmiHofRyzsBNexdtUOa861jjgDJjjpqK4a7ssutWCBPXgEAo1Frv",
+  type => "ssh-rsa",
+  user => "root",
+  require => File["/root/.ssh/id_rsa.pub"]
+}
+
--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -1,17 +1,59 @@
 class hadoop {
-    $hadoop_home = "/opt/hadoop"
+  $hadoop_home = "/opt/hadoop-1.0.4"
 
-    exec { "download_hadoop":
-      command => "/usr/bin/wget -O /tmp/hadoop.tar.gz http://apache.mirrors.timporter.net/hadoop/common/hadoop-1.0.4/hadoop-1.0.4.tar.gz",
-                 path => $hadoop_home,
-                 unless => "/bin/ls /opt | /bin/grep hadoop-1.0.4",
-                 require => Package["openjdk-6-jdk"]
-    }
+  exec { "download_hadoop":
+    command => "wget -O /tmp/hadoop.tar.gz http://apache.mirrors.timporter.net/hadoop/common/hadoop-1.0.4/hadoop-1.0.4.tar.gz",
+    path => '/usr/bin',
+    unless => "/bin/ls /opt | /bin/grep hadoop-1.0.4",
+    require => Package["openjdk-6-jdk"]
+  }
 
-    exec { "unpack_hadoop" :
-      command => "tar zvxf /tmp/hadoop.tar.gz -C /opt",
-      path => "/bin",
-      creates => "${hadoop_home}-1.0.4",
-      require => Exec["download_hadoop"]
-    }
+  exec { "unpack_hadoop" :
+    command => "tar zvxf /tmp/hadoop.tar.gz -C /opt",
+    path => "/bin",
+    creates => "${hadoop_home}",
+    require => Exec["download_hadoop"]
+  }
+  file {"${hadoop_home}/conf/slaves":
+      source => "puppet:///modules/hadoop/slaves",
+      mode => 644,
+      owner => root,
+      group => root,
+      require => Exec["unpack_hadoop"]
+  }
+  file {"${hadoop_home}/conf/masters":
+      source => "puppet:///modules/hadoop/masters",
+      mode => 644,
+      owner => root,
+      group => root,
+      require => Exec["unpack_hadoop"]
+  }
+  file {"$hadoop_home/conf/core-site.xml":
+      source => "puppet:///modules/hadoop/core-site.xml",
+      mode => 644,
+      owner => root,
+      group => root,
+      require => Exec["unpack_hadoop"]
+  }
+  file {"$hadoop_home/conf/mapred-site.xml":
+      source => "puppet:///modules/hadoop/mapred-site.xml",
+      mode => 644,
+      owner => root,
+      group => root,
+      require => Exec["unpack_hadoop"]
+  }
+  file {"$hadoop_home/conf/hdfs-site.xml":
+      source => "puppet:///modules/hadoop/hdfs-site.xml",
+      mode => 644,
+      owner => root,
+      group => root,
+      require => Exec["unpack_hadoop"]
+  }
+  file { "${hadoop_home}/conf/hadoop-env.sh":
+     source => "puppet:///modules/hadoop/hadoop-env.sh",
+     mode => 644,
+     owner => root,
+     group => root,
+     require => Exec["unpack_hadoop"]
+  }
 }
